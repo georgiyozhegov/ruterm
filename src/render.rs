43221edf9c2fdf439c61af875f389bf4161da3a9
George <georgiyozhegov.rs@gmail.com>
2024-04-19 14:30:02 +0400

    imports refactoring

--- src/render.rs
+++ src/render.rs
@@ -4,25 +4,24 @@ use crate::{
                 Direction,
         },
         error::Result,
-        io,
+        io::write_to,
 };
 use std::io::{
-        self as io_,
-        Read,
+        self,
         Write,
 };
 
-pub fn render_to(to: &mut dyn Write, text: String) -> Result<()>
+pub fn render_to(to: &mut dyn Write, text: Vec<String>) -> Result<()>
 {
-        for line in text.lines() {
-                io::write_to(to, line.as_bytes())?;
+        for line in text {
+                write_to(to, line.as_bytes())?;
                 cursor::move_(Direction::Down, 1)?;
                 cursor::move_(Direction::Left, line.len() as u16)?;
         }
         Ok(())
 }
 
-pub fn render(text: String) -> Result<()>
+pub fn render(text: Vec<String>) -> Result<()>
 {
-        render_to(&mut io_::stdout(), text)
+        render_to(&mut io::stdout(), text)
 }

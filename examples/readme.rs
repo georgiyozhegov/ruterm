89d58bfc23723deaffb174a2e3f48149727313a0
George <georgiyozhegov.rs@gmail.com>
2024-04-28 15:56:31 +0400

    readme example formatting

--- examples/readme.rs
+++ examples/readme.rs
@@ -1,22 +1,26 @@
 use ruterm::{
-    error::Result,
-    in_raw,
-    style::{color::fore, RESET},
-    io::write,
-    size,
-    cursor,
+        cursor,
+        error::Result,
+        in_raw,
+        io::write,
+        size,
+        style::{
+                color::fore,
+                RESET,
+        },
 };
 
-fn main() -> Result<()> {
-    in_raw!({
-        cursor::start()?; // clear screen
-        let (w, h) = size()?;
-        cursor::set(w / 2, h / 2)?; // move cursor to the center
-        write(fore::GREEN.as_bytes())?; // green foreground
-        write(b"Hello from raw mode!\n\r")?;
-        write(RESET.as_bytes())?; // reset style
-        cursor::set(0, h)?; // move cursor to the bottom
-    });
+fn main() -> Result<()>
+{
+        in_raw!({
+                cursor::start()?; // clear screen
+                let (w, h) = size()?;
+                cursor::set(w / 2, h / 2)?; // move cursor to the center
+                write(fore::GREEN.as_bytes())?; // green foreground
+                write(b"Hello from raw mode!\n\r")?;
+                write(RESET.as_bytes())?; // reset style
+                cursor::set(0, h)?; // move cursor to the bottom
+        });
 
-    Ok(())
+        Ok(())
 }

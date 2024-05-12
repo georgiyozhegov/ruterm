e6667c846d4cfec788920deeb28747d90d0b2305
George <georgiyozhegov.rs@gmail.com>
2024-05-12 12:15:44 +0400

    raw mode flags docs

--- src/raw.rs
+++ src/raw.rs
@@ -9,6 +9,7 @@ use std::{
 use termios::{
         tcsetattr,
         Termios as Termios_,
+        CS8,
         ECHO,
         ICANON,
         ICRNL,
@@ -63,11 +64,22 @@ impl Termios
                 })
         }
 
+        /// Enables raw mode.
+        ///
+        /// Sets values:
+        ///     - ECHO      disable echoing
+        ///     - ICANON    read byte-by-byte
+        ///     - ISIG      disable ctrl-c and ctrl-z exit
+        ///     - IXON      disable software flow control
+        ///     - IEXTEN    disable ctrl-v
+        ///     - ICRNL     fix ctrl-m
+        ///     - OPOST     disable output post-processing
         pub fn raw(&mut self) -> Result<()>
         {
                 self.raw.c_lflag &= !(ECHO | ICANON | IEXTEN | ISIG);
                 self.raw.c_iflag &= !(IXON | ICRNL);
                 self.raw.c_oflag &= !OPOST;
+                self.raw.c_cflag |= CS8;
                 self.raw.c_cc[VTIME] = 1;
                 self.raw.c_cc[VMIN] = 0;
                 tcsetattr(io::stdin().as_raw_fd(), TCSAFLUSH, &self.raw)
